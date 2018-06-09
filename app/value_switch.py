b665688d1ef91ea56b37531ae349d23766cc0b26
David Medina Ospina <davidmedinaospina@Davids-MacBook-Pro.local>
2018-06-09 16:23:01 +0200

    adding new models

--- app/value_switch.py
+++ app/value_switch.py
@@ -1,45 +1,32 @@
-def get_value_key_int(str_key):
-    if str_key == 'bloque_38':
-        return 0
-    if str_key == 'biblioteca':
-        return 1
-    if str_key == 'agora':
-        return 2
-    if str_key == 'dogger':
-        return 3
-    if str_key == 'admisiones':
-        return 4
-    if str_key == 'auditorio':
-        return 5
-    if str_key == 'bloque_18':
-        return 6
-    if str_key == 'administracion':
-        return 7
-    if str_key == 'idiomas':
-        return 8
-    if str_key == 'bloque_19':
-        return 9
-    return 10
-
 def get_value_key_str(int_key):
     if int_key == 0:
-        return 'bloque_38'
+        return 'Bloque_18'
+
     if int_key == 1:
-        return 'blioteca'
+        return 'Bloque_19'
+
     if int_key == 2:
-        return 'agora'
+        return 'Bloque_38'
+
     if int_key == 3:
-        return 'dogger'
+        return 'Bloque_26'
+
     if int_key == 4:
-        return 'admisiones'
+        return 'Admisiones'
+
     if int_key == 5:
-        return 'auditorio'
+        return 'Agora'
+
     if int_key == 6:
-        return 'bloque_18'
+        return 'Auditorio'
+
     if int_key == 7:
-        return 'administracion'
+        return 'Biblioteca'
+
     if int_key == 8:
-        return 'idiomas'
+        return 'Dogger'
+
     if int_key == 9:
-        return 'bloque_19'
+        return 'Idiomas'
+
     return 'Ningun lugar'
